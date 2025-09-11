1ce045a311309b78800a052495d67a69057b9c50
adamnasri123 <nasria@uwindsor.ca>
2025-09-11 10:28:58 -0400

    [1768] [Time Beats: 30.65%] [Memory Beats: 9.23%] - LeetPush

---
+++ Merge-Strings-Alternately.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string res;
+        res.reserve(word1.size() + word2.size()); // avoid reallocations
+
+        size_t i = 0, j = 0;
+        while (i < word1.size() || j < word2.size()) {
+            if (i < word1.size()) res.push_back(word1[i++]);
+            if (j < word2.size()) res.push_back(word2[j++]);
+        }
+        return res;
+    }
+};
