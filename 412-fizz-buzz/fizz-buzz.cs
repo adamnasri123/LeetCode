2d0c7ca50353967dcca0f00a6ec307931c63899b
adamnasri123 <nasria@uwindsor.ca>
2026-08-03 22:05:59 -0400

    Time: 3 ms (98.56%) | Memory: 70.3 MB (26.62%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public IList<string> FizzBuzz(int n) {
+        string[] arr = new string[n];      
+        for (int i = 1; i <= n; i++) {      
+            if (i % 3 == 0 && i % 5 == 0) {
+                arr[i - 1] = "FizzBuzz";    
+            }
+            else if (i % 3 == 0) {
+                arr[i - 1] = "Fizz";
+            }
+            else if (i % 5 == 0) {
+                arr[i - 1] = "Buzz";
+            }
+            else {
+                arr[i - 1] = i.ToString();
+            }
+        }
+        return arr;                        
+    }
+}
